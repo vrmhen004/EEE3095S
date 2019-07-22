f59a74c8a403765f84722105a2f5f0af00be749e
Hendri <vrmhen004@myuct.ac.za>
2019-07-22 13:32:08 +0100

    Added comments

--- Prac1/Prac1.py
+++ Prac1/Prac1.py
@@ -13,18 +13,25 @@ Date: 22/07/19
 import RPi.GPIO as GPIO
 import time
 
+#Globals
 binary = [0,0,0]
 index = 0
 
 # Logic that you write
 def main():
+	#Get globals locally
 	global binary
 	global index	
 	
+	#Get temp version of index for munipilation
 	temp = index
+	
+	#Reset counter
 	binary[0] = 0
 	binary[1] = 0 
 	binary[2] = 0
+	
+	#Fill bits
 	count = 2
 	while(temp > 0):
 		result = temp%2
@@ -35,7 +42,7 @@ def main():
 			binary[count] = 0
 		count = count - 1
 	
-	
+	#Check bit value and set led 
 	if binary[0] == 1:
 		GPIO.output(7, GPIO.HIGH)
 	else:
@@ -49,41 +56,47 @@ def main():
 	else:
 		GPIO.output(13, GPIO.LOW)
 		
-	#time.sleep(1);
-	
-	#index += 1;
-	
-	#if GPIO.event_detected(15):
-	#	index += 1
-	#if GPIO.event_detected(15):
-	#	index -= 1
-		
-	if index > 7:
-		index = 0
-	if index < 0:
-		index = 7
+	#time.sleep(1); #-- used for auto incrementation uncomment if switching use case
+	#index += 1; #-- used for auto incrementation uncomment if switching use case
 		
 def my_callback(channel):
+	#Get globals
 	global index
+	
+	#Check which channel is high and increase/decrease index accordingly
 	if channel == 15:
 		index += 1
 	if channel == 16:
 		index -= 1
+	
+	#Check for overflows
+	if index > 7:
+		index = 0
+	if index < 0:
+		index = 7
 
 
 # Only run the functions if 
 if __name__ == "__main__":
 	# Make sure the GPIO is stopped correctly
 	try:
+		#Setup GPIO
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setwarnings(False)
 		
+		#Setup Output Channels
 		channel = [7,11,13]
 		GPIO.setup(channel, GPIO.OUT)
 		GPIO.output(channel, GPIO.LOW)
+		
+		#Setup Input Channels
 		GPIO.setup([15,16], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		
+		#Add events
 		GPIO.add_event_detect(15, GPIO.RISING, callback=my_callback, bouncetime=200)
 		GPIO.add_event_detect(16, GPIO.RISING, callback=my_callback, bouncetime=200)
+		
+		#Setup is done here for efficiency reasons (Not too setup each time in loop
 			
 		while True:
 			main()
