19cbe039cc60c808483a8ac987cf208e626f36e2
Kyle du Plessis <kyleduplessis@gmail.com>
2019-09-12 11:42:39 +0200

    Reworked code - sound issue fixed and working

--- Prac4/src/Prac4.cpp
+++ Prac4/src/Prac4.cpp
@@ -1,10 +1,10 @@
 /*
  * Prac4.cpp
- * 
+ *
  * Originall written by Stefan Schröder and Dillion Heald
- * 
+ *
  * Adapted for EEE3096S 2019 by Keegan Crankshaw
- * 
+ *
  * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -20,233 +20,267 @@
 
 using namespace std;
 
-//changed to volatile for more thread concurrency
+long lastInterruptTime = 0; // used for button debounce
 
-volatile long lastInterruptTime = 0; //Used for button debounce
+bool playing = true;
 
-volatile bool playing = true; // should be set false when paused
-volatile bool stopped = false; // If set to true, program should close
+bool stopped = false;
+
 unsigned char buffer[2][BUFFER_SIZE][2];
-volatile int buffer_location = 0;
-volatile bool bufferReading = 0; //using this to switch between column 0 and 1 - the first column
-volatile bool threadReady = false; //using this to finish writing the first column at the start of the song, before the column is played
 
-char writeCommand = 0b00010000;
+int bufferLocation = 0;
+
+int bufferReading = 0;
+
+bool threadReady = false;
 
 // Configure your interrupts here.
 // Don't forget to use debouncing.
-void play_pause_isr(void){
-    //Write your logis here
-    
-    //Debounce
-    long interruptTime = millis();
-    
-    if (interruptTime - lastInterruptTime > 200) {
-    
-        if(playing){
-        	printf("Paused\n");
-        } else {
-        	printf("Resuming\n");
-        }
-	playing = !playing;
-	
-    }
+void play_pause_isr(void) {
 
-    lastInterruptTime = interruptTime;
+	//Write your logic here
+	//Debounce
+	long interruptTime = millis();
+
+	if (interruptTime - lastInterruptTime > 200) {
+
+		if (playing) {
+
+			printf("Paused\n");
+
+		} else {
+
+			printf("Resuming\n");
+
+		}
+
+		playing = !playing;
+
+
+	}
+
+	lastInterruptTime = interruptTime;
+
 }
 
-void stop_isr(void){
-    // Write your logic here
-    
-    //Debounce
-    long interruptTime = millis();
+void stop_isr(void) {
 
-    if (interruptTime - lastInterruptTime > 200) {
+	// Write your logic here
+	//Debounce
+	long interruptTime = millis();
 
-        printf("Stopped\n");
-	stopped = true;
-	
-    }
+	if (interruptTime - lastInterruptTime > 200) {
 
-    lastInterruptTime = interruptTime;
+		printf("Stopped\n");
+
+		stopped = true;
+
+	}
+
+	lastInterruptTime = interruptTime;
+
 }
 
 /*
- * Setup Function. Called once 
+ * Setup Function. Called once
  */
-int setup_gpio(void){
-    //Set up wiring Pi
-    wiringPiSetupPhys();
-    
-    //setting up the buttons
-    
-    //setup mode and pin number
-    pinMode(PLAY_BUTTON, INPUT);
-    pullUpDnControl(PLAY_BUTTON, PUD_UP);
-    pinMode(STOP_BUTTON, INPUT);
-    pullUpDnControl(STOP_BUTTON, PUD_UP);
-    
-    //add interrupts
-    wiringPiISR(PLAY_BUTTON, INT_EDGE_FALLING, &play_pause_isr);
-    wiringPiISR(STOP_BUTTON, INT_EDGE_FALLING, &stop_isr);
-    
-    //setting up the SPI interface
-    if(wiringPiSPISetup(SPI_CHAN, SPI_SPEED) == -1){
-    	return -1;
-    }
-    return 0;
+
+bool setup = false;
+
+int setup_gpio(void) {
+
+	if (setup) { // called once only
+
+		return 0;
+
+	}
+
+	//Set up wiring Pi
+	wiringPiSetupPhys();
+
+	//setting up the buttons
+	//setup mode and pin number
+	pinMode(PLAY_BUTTON, INPUT);
+
+	pullUpDnControl(PLAY_BUTTON, PUD_UP);
+
+	pinMode(STOP_BUTTON, INPUT);
+
+	pullUpDnControl(STOP_BUTTON, PUD_UP);
+
+	//adding interrupts
+	wiringPiISR(PLAY_BUTTON, INT_EDGE_FALLING, &play_pause_isr);
+
+	wiringPiISR(STOP_BUTTON, INT_EDGE_FALLING, &stop_isr);
+
+	//setting up the SPI interface
+	if (wiringPiSPISetup(SPI_CHAN, SPI_SPEED) != -1) {
+
+		printf("SPI interface setup complete.\n");
+
+	} else {
+
+		printf("SPI interface setup failed.\n");
+
+		return -1;
+
+	}
+
+	setup = true;
+
+	printf("GPIO setup complete.\n");
+
+	return 0;
+
 }
 
-/* 
+/*
  * Thread that handles writing to SPI
- * 
+ *
  * You must pause writing to SPI if not playing is true (the player is paused)
  * When calling the function to write to SPI, take note of the last argument.
  * You don't need to use the returned value from the wiring pi SPI function
  * You need to use the buffer_location variable to check when you need to switch buffers
  */
-void *playThread(void *threadargs){
-    // If the thread isn't ready, don't do anything
-    while(!threadReady)
-        continue;
-    
-    unsigned char data[2]; // for testing
-    unsigned char d = 0; // for testing
-    
-    //You need to only be playing if the stopped flag is false
-    while(!stopped){
-    	d++; // for testing
-        //Code to suspend playing if paused
-	if(!playing){
-		//wait until unpaused		
-		continue;
-	}	
-        
-        /* TEST 
-        data[0] = (writeCommand | (d >> 6)); 
-        data[1] = (d << 2); 
-        wiringPiSPIDataRW(SPI_CHAN, data, 2);
-         TEST */
-        
-        //Write the buffer out to SPI
-       // wiringPiSPIDataRW(SPI_CHAN, buffer[bufferReading][buffer_location], 2);
-		
-        //Do some maths to check if you need to toggle buffers
-        buffer_location++;
-        if(buffer_location >= BUFFER_SIZE) {
-            buffer_location = 0;
-            bufferReading = !bufferReading; // switches column one it finishes one column
-        }
-    }
-    threadReady = false;
-    
-    printf("Play thread exited\n");
-    
-    pthread_exit(NULL);
-    
+
+PI_THREAD (play_audio) {
+
+	while(!threadReady) {
+
+		delay(1);
+
+	}
+
+	while(!stopped) {
+
+		while(!playing) {
+
+			delay(50);
+
+		}
+
+		wiringPiSPIDataRW(SPI_CHAN, buffer[bufferReading][bufferLocation], 2);
+
+		bufferLocation++;
+
+		piLock(0);
+
+		if(bufferLocation >= BUFFER_SIZE) {
+
+			bufferLocation = 0;
+
+			bufferReading = (bufferReading+1) % 2;
+
+		}
+
+		piUnlock(0);
+
+	}
+
+	printf("Thread exiting.\n");
+
+	return(0);
+
 }
 
-int main(){
-    // Call the setup GPIO function
-    if(setup_gpio()==-1){
-        return 0;
-    }
-    
-    /* Initialize thread with parameters
-     * Set the play thread to have a 99 priority
-     * Read https://docs.oracle.com/cd/E19455-01/806-5257/attrib-16/index.html
-     */ 
-    
-    //Write your logic here
-    pthread_attr_t tattr;
-    pthread_t thread_id;
-    int newprio = 99;
-    sched_param param;
-    
-    pthread_attr_init (&tattr);
-    pthread_attr_getschedparam (&tattr, &param); /* safe to get existing scheduling param */
-    param.sched_priority = newprio; /* set the priority; others are unchanged */
-    pthread_attr_setschedparam (&tattr, &param); /* setting the new scheduling param */
-    pthread_create(&thread_id, &tattr, playThread, (void *)1); /* with new priority specified *
-    
-    /*
-     * Read from the file, character by character
-     * You need to perform two operations for each character read from the file
-     * You will require bit shifting
-     * 
-     * buffer[bufferWriting][counter][0] needs to be set with the control bits
-     * as well as the first few bits of audio
-     * 
-     * buffer[bufferWriting][counter][1] needs to be set with the last audio bits
-     * 
-     * Don't forget to check if you have pause set or not when writing to the buffer
-     * 
-     */
-     
-    // Open the file
-    char ch;
-    FILE *filePointer;
-    printf("%s\n", FILENAME);
-    filePointer = fopen(FILENAME, "r"); // read mode
+int main() {
 
-    if (filePointer == NULL) {
-        perror("Error while opening the file.\n");
-        exit(EXIT_FAILURE);
-    }
+	// Call the setup GPIO function
+	if (setup_gpio() == -1) {
 
-    int counter = 0;
-    int bufferWriting = 0;
+		return 0;
 
-    // Have a loop to read from the file
-    while((ch = fgetc(filePointer)) != EOF){
-        while(threadReady && bufferWriting==bufferReading && counter==0){
-            //waits in here after it has written to a side, and the thread is still reading from the other side
-            continue;
-        }
-        
-        if(stopped){
-        	break;
-        }
-        
-        unsigned char data = fgetc(filePointer);
-        //printf("Write %d\n", data);
-        
-        //Set config bits for first 8 bit packet and OR with upper bits
-       // printf("Data %d\n", data);
-        buffer[bufferWriting][counter][0] = (writeCommand | (data >> 6)); 
-       // printf("MSB %d\n", buffer[bufferWriting][counter][1]);
-        //Set next 8 bit packet
-        buffer[bufferWriting][counter][1] = (data << 2); 
-       // printf("LSB %d\n", buffer[bufferWriting][counter][1]);
+	}
 
-        counter++;
-        if(counter >= BUFFER_SIZE+1){
-            if(!threadReady){ 
-                threadReady = true;
-            }
+	/* Initialize thread with parameters
+	 * Set the play thread to have a 99 priority
+	 * Read https://docs.oracle.com/cd/E19455-01/806-5257/attrib-16/index.html
+	 */
 
-            counter = 0;
-            bufferWriting = (bufferWriting+1)%2;
-        }
+	//Write your logic here
+	piThreadCreate (play_audio);
 
-    }
-     
-    // Close the file
-    fclose(filePointer);
-    printf("Complete reading\n"); 
-	 
-    //Join and exit the playthread
-    printf("Waiting on playing thread\n"); 
-    pthread_join(thread_id, NULL); 
-    
-    printf("Turning off DAC\n");
-    unsigned char data[2] = {0,0};
-    wiringPiSPIDataRW(SPI_CHAN, data, 2);
-    
-    printf("Exiting\n"); 
-    
-    //pthread_exit(NULL); - this doesn't stop program so removed it
-	
-    return 0;
+	// Open the file
+	char ch;
+
+	FILE *filePointer;
+
+	printf("%s\n", FILENAME);
+
+	filePointer = fopen(FILENAME, "r"); // read mode
+
+	if (filePointer == NULL) {
+
+		perror("Error while opening the file.\n");
+
+		exit(EXIT_FAILURE);
+
+	}
+
+	/*
+	 * Read from the file, character by character
+	 * You need to perform two operations for each character read from the file
+	 * You will require bit shifting
+	 *
+	 * buffer[bufferWriting][counter][0] needs to be set with the control bits
+	 * as well as the first few bits of audio
+	 *
+	 * buffer[bufferWriting][counter][1] needs to be set with the last audio bits
+	 *
+	 * Don't forget to check if you have pause set or not when writing to the buffer
+	 *
+	 */
+
+	int counter = 0;
+
+	int bufferWriting = 0;
+
+	char config = 0 * 0x40 + 1 * 0x20 + 1 * 0x10;
+
+	printf("\nPlaying song >>>\n");
+
+	// Have a loop to read from the file
+	while ((ch = fgetc(filePointer)) != EOF) {
+
+		while (threadReady && (bufferWriting == bufferReading) && (counter == 0)) {
+
+			delay(1);
+
+		}
+
+		//Set config bits
+		buffer[bufferWriting][counter][0] = ch >> 6;
+
+		buffer[bufferWriting][counter][0] |= config;
+
+		buffer[bufferWriting][counter][1] = ch << 2;
+
+		counter++;
+
+		piLock(0);
+
+		if (counter >= BUFFER_SIZE) {
+
+			if (!threadReady) {
+
+				threadReady = true;
+
+			}
+
+			counter = 0;
+
+			bufferWriting = (bufferWriting + 1) % 2;
+
+		}
+
+		piUnlock(0);
+
+	}
+
+	fclose(filePointer); // Close the file
+
+	printf("Reading done.");
+
+	return 0;
 }
 
--- Prac4/src/Prac4.h
+++ Prac4/src/Prac4.h
@@ -2,7 +2,7 @@
  * Prac4.cpp
  *
  * Written for EEE3096S 2019 by Keegan Crankshaw
- * 
+ *
  * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -15,39 +15,47 @@
  */
 
 #ifndef PRAC4_H
+
 #define PRAC4_H
 
 //Includes
 #include <wiringPi.h>
+
 #include <vector>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <wiringPiSPI.h>
+
 #include <unistd.h>
+
 #include <math.h>
+
 #include <pthread.h>
+
 #include <iostream>
 
 //Define buttons
-#define PLAY_BUTTON 29// Write your value here
-#define STOP_BUTTON 31// Write your value here
-#define BUFFER_SIZE 1000
+#define PLAY_BUTTON 29 // Write your value here
+
+#define STOP_BUTTON 31 // Write your value here
+
+const int BUFFER_SIZE = 500;
 
 //SPI Settings
-#define SAMPLERATE 16000 //16k
-#define WIDTH 8 //bit
-#define NO_CHANNELS 1 //?
-#define SPI_CHAN 0// Write your value here
-#define SPI_SPEED SAMPLERATE*WIDTH*NO_CHANNELS*8/5 // Write your value here
+#define SPI_CHAN 0 // Write your value here
+
+const int SPI_SPEED = 16000 * 16 * 1 * 8/5; // Write your value here
 
 //Filename
 #define FILENAME "src/sound_16k_8bit.raw"
 
-//Function definitions
-void play_audio(void);
 void stop_audio(void);
-int setup_gpio(void);
-int main(void);
 
+int setup_gpio(void);
+
+int main(void);
 
 #endif
